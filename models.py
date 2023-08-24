50042a5ec7f8f40b5fa545a1ea517cd41ef82f24
akasharunabharathi <90937878+akasharunabharathi@users.noreply.github.com>
2023-08-24 18:41:26 +0000

    Updated code to use HuggingFace Hub instead of HuggingFace Pipeline.

--- models.py
+++ models.py
@@ -1,40 +1,47 @@
 """
 This module is responsible for handling user input and explaining their reports to them.
 """
-from langchain.llms import HuggingFacePipeline
-from langchain.prompts import PromptTemplate
+from langchain import HuggingFaceHub
+from langchain import PromptTemplate, LLMChain
+from decouple import config
+import os
 import ocr
 
-llm = HuggingFacePipeline.from_model_id(
-    model_id="google/pegasus-xsum",
-    task="summarization",
-    model_kwargs={"temperature": 0, "max_length": 256},
-)
+os.environ = config("HUGGINGFACE_HUB_API")
 
 def bio_summarize(file_name: str):
     """
     Accepts a pdf file or an image as an input. Input is, additionally, a medical report. 
     This function reads the report, summairzes it for the user, and returns the summary.
     """
-    is_pdf_file = file_name.contains(".pdf")
-    is_image_file = file_name.contains(".jpeg") or file_name.contains(".jpg") or file_name.contains(".heic") or file_name.contains(".png")
-    report = None
-    
+    llm = HuggingFaceHub(
+    repo_id ="google/pegasus-xsum",
+    task = "summarization",
+    model_kwargs = {"temperature": 0, "max_length": 256},
+    )
+
     template_message = """You are a nurse at a hospital, and are now dealing with patients 
     that don't know biolgy, and consequently, don't fully understand their reports. Your job 
     is to explain their reports to them, in a simple, reassuring way. Here is their report:\n\n{report_string}"""
-    
+
     prompt_template = PromptTemplate.from_template(
         input_variables = ["report_string"],
         template = template_message
     )
+
+    llm_chain = LLMChain(prompt = prompt_template, llm = llm)
+    
+    is_pdf_file = file_name.contains(".pdf")
+    is_image_file = file_name.contains(".jpeg") or file_name.contains(".jpg") or file_name.contains(".heic") or file_name.contains(".png")
+    report = None
 
     if is_pdf_file:
         pass
     elif is_image_file:
         report = ocr.image_report(file_name)
         print(report)
-        prompt_template.format(report_string = report)
+        print("\n\n")
+        return llm_chain.run(report)
     else:
         pass
 
--- ocr.py
+++ ocr.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 
 # If you don't have tesseract executable in your PATH, include the following:
-pytesseract.pytesseract.tesseract_cmd = r'usr/bin/tesseract'
+# pytesseract.pytesseract.tesseract_cmd = r'usr/bin/tesseract'
 # Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 
 def image_report(file_name: str):
