fc59c7f41a4f169edb03e8b02ee0b6ddb0922723
akasharunabharathi <90937878+akasharunabharathi@users.noreply.github.com>
2023-08-25 02:37:38 +0000

    It works!

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import models
 file_name = "medicalese/Sample Report.png"
 
 if __name__ == "__main__":
-    print(ocr.image_report(file_name))
-    # print(models.bio_summarize(file_name))
+    # print(ocr.image_report(file_name))
+    print(models.bio_summarize(file_name))
--- models.py
+++ models.py
@@ -2,12 +2,13 @@
 This module is responsible for handling user input and explaining their reports to them.
 """
 from langchain import HuggingFaceHub
+from langchain.chat_models import ChatOpenAI
 from langchain import PromptTemplate, LLMChain
 from decouple import config
 import os
 import ocr
 
-os.environ = config("HUGGINGFACE_HUB_API")
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 
 def bio_summarize(file_name: str):
     """
@@ -17,32 +18,34 @@ def bio_summarize(file_name: str):
     llm = HuggingFaceHub(
     repo_id ="google/pegasus-xsum",
     task = "summarization",
-    model_kwargs = {"temperature": 0, "max_length": 256},
+    model_kwargs = {"temperature": 0, "max_length": 1024},
     )
+
+    is_pdf_file = ".pdf" in file_name
+    image_types = [".jpeg", ".jpg", ".heic", ".png"]
+    is_image_file = any(image_type in file_name for image_type in image_types)
+    # is_image_file = file_name.contains(".jpeg") or file_name.contains(".jpg") or file_name.contains(".heic") or file_name.contains(".png")
+    report = None
 
     template_message = """You are a nurse at a hospital, and are now dealing with patients 
     that don't know biolgy, and consequently, don't fully understand their reports. Your job 
-    is to explain their reports to them, in a simple, reassuring way. Here is their report:\n\n{report_string}"""
+    is to explain their reports to them, in a simpler manner, without the biological terms, i.e. in layman terms. 
+    Help them understand what is happening with their bodies.
+    Here is their report:\n\n{report_string}"""
 
     prompt_template = PromptTemplate.from_template(
-        input_variables = ["report_string"],
         template = template_message
     )
 
     llm_chain = LLMChain(prompt = prompt_template, llm = llm)
-    
-    is_pdf_file = file_name.contains(".pdf")
-    is_image_file = file_name.contains(".jpeg") or file_name.contains(".jpg") or file_name.contains(".heic") or file_name.contains(".png")
-    report = None
 
     if is_pdf_file:
         pass
     elif is_image_file:
         report = ocr.image_report(file_name)
-        print(report)
-        print("\n\n")
+        prompt_template.format(report_string = report)
         return llm_chain.run(report)
     else:
         pass
 
-    return llm(prompt_template)
+    return None
