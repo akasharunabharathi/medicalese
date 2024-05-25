fad7c3cb045303a6fae8e8731a691d1900ae75e8
Akash Arunabharathi <90937878+akasharunabharathi@users.noreply.github.com>
2024-05-25 22:57:09 +0000

    Added API url to frontend

--- medicalese-frontend/app/components/inputPanel.tsx
+++ medicalese-frontend/app/components/inputPanel.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {useState, ChangeEvent} from 'react';
-import styles from '@/app/ui/inputPanel.module.css'
+import styles from '@/app/ui/panel.module.css'
 
 export default function InputPanel() {
     const [image, setImage] = useState<File|null>(null);
     const [imageURL, setImageURL] = useState<string | null>(null);
     const [summary, setSummary] = useState<string | null>(null);
-    const apiURL = "hello";
+    const apiURL = "ac52903aeda2d4b7f9c7f759692ec94f-292991886.us-east-1.elb.amazonaws.com";
 
     const handleImageChange = (event:ChangeEvent<HTMLInputElement>) => {
         const fileDetails = event.target.files ? event.target.files[0] : null;
@@ -36,13 +36,15 @@ export default function InputPanel() {
             // At this point, this response should contain the summary of the report
             // access the summary from the body of the response and use setSummary here.
             // This component will be re-rendered, at which point we can display only the summary
+            const data = await response.json()
+            setSummary(data.summary);
             alert("Processing successful");
         } else {
             alert("Processing failed");
         }
     }
     // On uploading the image, basically this says that 
-    return <>
+    return <div className={styles.master_div}>
     <div className={styles.uploadArea}>
         <div>
             {imageURL ? 
@@ -58,7 +60,14 @@ export default function InputPanel() {
         <label htmlFor = 'file-input' className={styles.fileInput}>Choose your file</label>
         <div className={styles.elemSpace}></div>
         <button className={styles.button} onClick={handleImageUpload}>Upload</button>
+        </div>
+
+        {summary && (
+            <div className= {styles.outputPanel}>
+                <h3>Summary</h3>
+                <p>{summary}</p>
+            </div>
+        )}
     </div>
     </div>
-    </>
 }
